75805d580321f043a08c3f529130b81ad8e9b27b
Patrick Kelly <patrick.kelly@sherwin.com>
2016-05-27 06:30:20 -0400

    Added reset feature

--- elm_buttons_01/Main.elm
+++ elm_buttons_01/Main.elm
@@ -6,12 +6,13 @@ import Html.Events exposing (onClick)
 
 main = Html.beginnerProgram {model = 0, view = view, update = update}
 
-type Msg = Increment | Decrement
+type Msg = Increment | Decrement | Reset
 
 update msg model = 
     case msg of
         Increment -> model + 1
         Decrement -> model - 1
+        Reset -> 0
 
 view model = 
     
@@ -20,4 +21,5 @@ view model =
         button [onClick Decrement] [text "-"]
         ,div [] [text (toString model)]
         ,button [onClick Increment] [text "+"]
+        ,button [onClick Reset] [text "Reset"]
     ]     
