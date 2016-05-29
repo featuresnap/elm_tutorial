6b5b475ce524cbdcf5857e8ad3b2321844d6a5d1
Patrick Kelly <patrick.kelly@sherwin.com>
2016-05-29 06:56:17 -0400

    basic password validation example working

---
+++ 03_elm_form/Main.elm
@@ -0,0 +1,51 @@
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.App as App
+import Html.Events exposing (onInput)
+
+main =   
+    App.beginnerProgram {model = model, update = update, view = view}
+
+
+type alias Model = {
+    name : String,
+    password : String,
+    passwordAgain : String
+}
+
+type Msg 
+    = Name String
+    | Password String
+    | PasswordAgain String
+
+model : Model
+model = 
+    Model "" "" ""
+
+update : Msg -> Model -> Model
+update action model = 
+    case action of 
+    Name name -> {model | name = name}
+    Password password -> {model | password = password}
+    PasswordAgain password -> {model | passwordAgain = password}
+    
+view : Model -> Html Msg
+view model = 
+    div [] 
+    [
+        input [type' "text", placeholder "Name", onInput Name] [],
+        input [type' "text", placeholder "Password", onInput Password] [],
+        input [type' "text", placeholder "Re-Enter Password", onInput PasswordAgain] [],
+        viewValidation model
+    ] 
+
+viewValidation : Model -> Html Msg
+viewValidation model = 
+    let (color, message) = 
+        if model.password == model.passwordAgain then
+            ("green", "OK")
+        else
+            ("red", "Passwords do not match")
+    in 
+    div [ style [("color", color)]] [ text message ]
+    
